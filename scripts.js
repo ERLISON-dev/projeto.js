327958469896e2e884c58b05f9c2a3a01b075761
ERLISON-dev <83833883+ERLISON-dev@users.noreply.github.com>
2021-05-17 19:43:48 -0300

    esboços de js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+   //variaveis e tipos de dados
+//declaração or Declaration
+var name
+
+// Assignament or atribuiçao de valores
+name = "elinho"
+//que tipo de dado foi colocdo na variavel
+console.log(typeof name)
